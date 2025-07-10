e6b261ffd8109b9b60e9da0c0453f442c8eefd56
gamoradima <gamoradima@gmail.com>
2025-07-10 17:49:30 +0300

    Realty section added, lookups filled in

---
+++ UsrRealty/Schemas/UsrRealtyTypeFormPage/UsrRealtyTypeFormPage.js
@@ -0,0 +1,124 @@
+define("UsrRealtyTypeFormPage", /**SCHEMA_DEPS*/[]/**SCHEMA_DEPS*/, function/**SCHEMA_ARGS*/()/**SCHEMA_ARGS*/ {
+	return {
+		viewConfigDiff: /**SCHEMA_VIEW_CONFIG_DIFF*/[
+			{
+				"operation": "insert",
+				"name": "Input_nrhqpgm",
+				"values": {
+					"layoutConfig": {
+						"column": 1,
+						"colSpan": 1,
+						"row": 1,
+						"rowSpan": 1
+					},
+					"type": "crt.Input",
+					"multiline": false,
+					"label": "$Resources.Strings.UsrRealtyTypeDS_Name_orm8f0h",
+					"labelPosition": "above",
+					"control": "$UsrRealtyTypeDS_Name_orm8f0h"
+				},
+				"parentName": "MainContainer",
+				"propertyName": "items",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "ColorPicker_mu9x99w",
+				"values": {
+					"layoutConfig": {
+						"column": 1,
+						"colSpan": 1,
+						"row": 2,
+						"rowSpan": 1
+					},
+					"type": "crt.ColorPicker",
+					"label": "$Resources.Strings.UsrRealtyTypeDS_UsrMyColor_8rqd47j",
+					"labelPosition": "above",
+					"control": "$UsrRealtyTypeDS_UsrMyColor_8rqd47j",
+					"pickerMode": "extended"
+				},
+				"parentName": "MainContainer",
+				"propertyName": "items",
+				"index": 1
+			},
+			{
+				"operation": "insert",
+				"name": "Input_kmjg07u",
+				"values": {
+					"layoutConfig": {
+						"column": 1,
+						"colSpan": 1,
+						"row": 3,
+						"rowSpan": 1
+					},
+					"type": "crt.Input",
+					"multiline": false,
+					"label": "$Resources.Strings.UsrRealtyTypeDS_Description_3ihbs1w",
+					"labelPosition": "above",
+					"control": "$UsrRealtyTypeDS_Description_3ihbs1w"
+				},
+				"parentName": "MainContainer",
+				"propertyName": "items",
+				"index": 2
+			}
+		]/**SCHEMA_VIEW_CONFIG_DIFF*/,
+		viewModelConfigDiff: /**SCHEMA_VIEW_MODEL_CONFIG_DIFF*/[
+			{
+				"operation": "merge",
+				"path": [
+					"attributes"
+				],
+				"values": {
+					"UsrRealtyTypeDS_Name_orm8f0h": {
+						"modelConfig": {
+							"path": "UsrRealtyTypeDS.Name"
+						}
+					},
+					"UsrRealtyTypeDS_UsrMyColor_8rqd47j": {
+						"modelConfig": {
+							"path": "UsrRealtyTypeDS.UsrMyColor"
+						}
+					},
+					"UsrRealtyTypeDS_Description_3ihbs1w": {
+						"modelConfig": {
+							"path": "UsrRealtyTypeDS.Description"
+						}
+					}
+				}
+			}
+		]/**SCHEMA_VIEW_MODEL_CONFIG_DIFF*/,
+		modelConfigDiff: /**SCHEMA_MODEL_CONFIG_DIFF*/[
+			{
+				"operation": "merge",
+				"path": [],
+				"values": {
+					"dataSources": {
+						"UsrRealtyTypeDS": {
+							"type": "crt.EntityDataSource",
+							"scope": "page",
+							"config": {
+								"entitySchemaName": "UsrRealtyType",
+								"loadParameters": {
+									"options": {
+										"pagingConfig": {
+											"rowCount": 1,
+											"rowsOffset": -1
+										},
+										"sortingConfig": {
+											"columns": []
+										}
+									}
+								},
+								"allowCopyingRecords": false
+							}
+						}
+					},
+					"primaryDataSourceName": "UsrRealtyTypeDS"
+				}
+			}
+		]/**SCHEMA_MODEL_CONFIG_DIFF*/,
+		handlers: /**SCHEMA_HANDLERS*/[]/**SCHEMA_HANDLERS*/,
+		converters: /**SCHEMA_CONVERTERS*/{}/**SCHEMA_CONVERTERS*/,
+		validators: /**SCHEMA_VALIDATORS*/{}/**SCHEMA_VALIDATORS*/
+	};
+});
---
+++ UsrRealty/Schemas/UsrRealty_ListPage/UsrRealty_ListPage.js
@@ -0,0 +1,154 @@
+define("UsrRealty_ListPage", /**SCHEMA_DEPS*/[]/**SCHEMA_DEPS*/, function/**SCHEMA_ARGS*/()/**SCHEMA_ARGS*/ {
+	return {
+		viewConfigDiff: /**SCHEMA_VIEW_CONFIG_DIFF*/[
+			{
+				"operation": "merge",
+				"name": "MenuItem_ImportFromExcel",
+				"values": {
+					"clicked": {
+						"request": "crt.ImportDataRequest",
+						"params": {
+							"entitySchemaName": "UsrRealty"
+						}
+					}
+				}
+			},
+			{
+				"operation": "merge",
+				"name": "FolderTree",
+				"values": {
+					"rootSchemaName": "UsrRealty"
+				}
+			},
+			{
+				"operation": "merge",
+				"name": "DataTable",
+				"values": {
+					"columns": [
+						{
+							"id": "f252f581-0ccf-44ac-b7c9-c00df2ad9919",
+							"code": "PDS_UsrName",
+							"caption": "#ResourceString(PDS_UsrName)#",
+							"dataValueType": 1,
+							"width": 169
+						},
+						{
+							"id": "7b5aed53-4031-763d-694b-5c8bb40f3c5a",
+							"code": "PDS_UsrPrice",
+							"caption": "#ResourceString(PDS_UsrPrice)#",
+							"dataValueType": 32,
+							"width": 168
+						},
+						{
+							"id": "c3d2f2ef-546a-6744-4837-9485a1d01ab1",
+							"code": "PDS_UsrType",
+							"caption": "#ResourceString(PDS_UsrType)#",
+							"dataValueType": 10,
+							"width": 158
+						},
+						{
+							"id": "52765f70-ed00-c57e-696c-c65fa47530b5",
+							"code": "PDS_UsrOfferType",
+							"caption": "#ResourceString(PDS_UsrOfferType)#",
+							"dataValueType": 10,
+							"width": 135
+						},
+						{
+							"id": "5f4478a7-1af8-81cc-f56f-ba5487dd20f4",
+							"code": "PDS_CreatedOn",
+							"caption": "#ResourceString(PDS_CreatedOn)#",
+							"dataValueType": 7
+						}
+					]
+				}
+			},
+			{
+				"operation": "merge",
+				"name": "Dashboards",
+				"values": {
+					"_designOptions": {
+						"entitySchemaName": "UsrRealty",
+						"dependencies": [
+							{
+								"attributePath": "Id",
+								"relationPath": "PDS.Id"
+							}
+						],
+						"filters": []
+					}
+				}
+			}
+		]/**SCHEMA_VIEW_CONFIG_DIFF*/,
+		viewModelConfigDiff: /**SCHEMA_VIEW_MODEL_CONFIG_DIFF*/[
+			{
+				"operation": "merge",
+				"path": [
+					"attributes",
+					"Items",
+					"viewModelConfig",
+					"attributes"
+				],
+				"values": {
+					"PDS_UsrName": {
+						"modelConfig": {
+							"path": "PDS.UsrName"
+						}
+					},
+					"PDS_UsrPrice": {
+						"modelConfig": {
+							"path": "PDS.UsrPrice"
+						}
+					},
+					"PDS_UsrType": {
+						"modelConfig": {
+							"path": "PDS.UsrType"
+						}
+					},
+					"PDS_UsrOfferType": {
+						"modelConfig": {
+							"path": "PDS.UsrOfferType"
+						}
+					},
+					"PDS_CreatedOn": {
+						"modelConfig": {
+							"path": "PDS.CreatedOn"
+						}
+					}
+				}
+			}
+		]/**SCHEMA_VIEW_MODEL_CONFIG_DIFF*/,
+		modelConfigDiff: /**SCHEMA_MODEL_CONFIG_DIFF*/[
+			{
+				"operation": "merge",
+				"path": [
+					"dataSources",
+					"PDS",
+					"config"
+				],
+				"values": {
+					"entitySchemaName": "UsrRealty",
+					"attributes": {
+						"UsrName": {
+							"path": "UsrName"
+						},
+						"UsrPrice": {
+							"path": "UsrPrice"
+						},
+						"UsrType": {
+							"path": "UsrType"
+						},
+						"UsrOfferType": {
+							"path": "UsrOfferType"
+						},
+						"CreatedOn": {
+							"path": "CreatedOn"
+						}
+					}
+				}
+			}
+		]/**SCHEMA_MODEL_CONFIG_DIFF*/,
+		handlers: /**SCHEMA_HANDLERS*/[]/**SCHEMA_HANDLERS*/,
+		converters: /**SCHEMA_CONVERTERS*/{}/**SCHEMA_CONVERTERS*/,
+		validators: /**SCHEMA_VALIDATORS*/{}/**SCHEMA_VALIDATORS*/
+	};
+});
